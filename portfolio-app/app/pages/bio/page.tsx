24bc347148e1379c5f150533f01f02ef8e14c4bf
Devroop Kar <kardevroop@gmail.com>
2024-06-26 22:07:50 -0400

    Added two pages and navbar

--- portfolio-app/app/pages/bio/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BioPage = () => {
-  return (
-    <div>BioPage</div>
-  )
-}
-
-export default BioPage
--- portfolio-app/components/Grid.tsx
+++ portfolio-app/components/Grid.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {gridItems} from '@/data'
+import { BentoGrid, BentoGridItem } from './ui/bento-grid'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id="about">
+        <BentoGrid>
+            {gridItems.map(({id, title, description, className,
+                img, imgClassName, titleClassName, spareImg
+            }) => (
+                    <BentoGridItem
+                        id={id}
+                        key={id}
+                        title={title}
+                        description={description}
+                        className={className}
+                        img={img}
+                        imgClassName={imgClassName}
+                        titleClassName={titleClassName}
+                        spareImg={spareImg}
+                    />
+            ))}
+        </BentoGrid>
+    </section>
   )
 }
 
